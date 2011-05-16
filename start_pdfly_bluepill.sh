40e5fd80809cbc2ad2d28e2d84fd364ee56ae303
Matthias Orgler <matthias@dreimannzelt.de>
2011-05-16 10:51:48 +0200

    correct paths for pdfly script

--- start_pdfly_bluepill.sh
+++ start_pdfly_bluepill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /var/lib/jenkins/pdfly
-source /var/lib/jenkins/.rvm/scripts/rvm
+cd /Users/morgler/code/pdfly
+source /Users/morgler/.rvm/scripts/rvm
 rvm use 1.9.2@pdfly
 sudo bluepill load config/pdfly.pill
